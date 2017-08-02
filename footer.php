00982ed2d37781ea4a1add83d714a45dc28729e4
andrew-macpherson <andrewmac17@gmail.com>
2017-08-02 17:02:35 -0400

    updates

--- footer.php
+++ footer.php
@@ -1,3 +1,11 @@
+	<footer>
+
+		<section class="footer-copy">
+			<span>Copyright 2017 Industrial Safety Trainers, All Right Reserved</span>
+		</section>
+
+	</footer>
+
 	<?php wp_footer(); ?>
 	</body>
 </html>
--- functions.php
+++ functions.php
@@ -6,3 +6,28 @@ function industrial_safety_trainers_scripts() {
     wp_enqueue_style( 'style-name', get_stylesheet_directory_uri().'/src/css/main.css',array('bootstrap') );
 }
 add_action( 'wp_enqueue_scripts', 'industrial_safety_trainers_scripts' );
+
+
+
+
+//ELEMENT SHORTCODE
+function element($atts,$content){
+	extract( shortcode_atts( array(
+		'tag'			=> 'div',
+        'class' 		=> ''
+    ), $atts ) );
+
+
+    $content = do_shortcode( shortcode_unautop( $content ) );
+    $content = stripParagraphs($content);
+
+    $return = '';
+	$return .= '<'.$tag.' class=" '.$class.'">';
+        $return .= $content;
+    $return .= '</'.$tag.'>';
+
+	return $return;
+}
+add_shortcode('el_1','element');
+add_shortcode('el_2','element');
+add_shortcode('el_3','element');
