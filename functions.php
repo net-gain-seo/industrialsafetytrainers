ca0be764e05d58a5fe68f83d86a7fb741be3e64a
andrew-macpherson <andrewmac17@gmail.com>
2017-08-16 15:47:27 -0400

    remove child theme set

--- functions.php
+++ functions.php
@@ -1,5 +1,21 @@
 <?php 
 
+//ADD THUMBNAIL SUPPORT
+add_theme_support('post-thumbnails');
+
+//REGISTER MAIN MENU
+add_action('init','register_site_menus');
+function register_site_menus(){
+    register_nav_menus(array(
+        'main' => 'Main Menu'
+    ));
+}
+
+
+
+
+
+
 
 // ENQUEUE STYLES AND SCRIPTS
 function industrial_safety_trainers_scripts() {
