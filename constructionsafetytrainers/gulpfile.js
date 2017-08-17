f5baedb4ed12404dfaba77eead5c08447608ae20
andrew-macpherson <andrewmac17@gmail.com>
2017-08-17 10:13:32 -0400

    git directory change

---
+++ constructionsafetytrainers/gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+// .scss input
+var cssSrc  = './src/scss/**/*.scss';
+// .css output
+var cssDest = './src/css';
+
+// Options
+var sassOptions = {
+  errLogToConsole: true,
+  includePaths: [cssSrc],
+  outputStyle: 'nested'
+};
+
+// Default options for autoprefixer are:
+// Browsers with over 1% market share,
+// Last 2 versions of all browsers,
+// Firefox ESR,
+// Opera 12.1
+//
+// Example options:
+// var autoprefixerOptions = {
+//   browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+// };
+
+gulp.task('sass', function () {
+  return gulp
+    .src(cssSrc)
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(sourcemaps.write('./'))
+    // .pipe(autoprefixer()) // use default options (see above)
+    .pipe(gulp.dest(cssDest));
+});
+
+gulp.task('watch', function() {
+  return gulp
+    // Watch the input folder for change,
+    // and run `sass` task when something happens
+    .watch('./src/scss/**', ['sass'])
+    // When there is a change,
+    // log a message in the console
+    .on('change', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
+
+gulp.task('default', ['sass', 'watch']);
+
+// The array passed as second argument of the task(..) function is a list of dependency tasks. Basically, it tells Gulp to run those tasks before running the one specified as a third argument (if any).
---
+++ industrialsafetytrainers/index.php
@@ -0,0 +1,65 @@
+<?php get_header(); ?>
+
+<main role="main">
+	<!-- section -->
+		<section class="banner-section">
+			<div class="banner-content d-flex justify-content-center align-items-stretch">
+				<div>
+					<div class="d-flex align-items-center fifty-percent-section right">
+						<h1>WHAT’S NEW?</h1>
+					</div>
+				</div>
+				<div></div>
+			</div>
+			
+			<img width="2000" height="375" src="http://localhost:81/industrialsafetytrainers/wp-content/uploads/2017/08/whats-new.png" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="" srcset="http://localhost:81/industrialsafetytrainers/wp-content/uploads/2017/08/whats-new.png 2000w, http://localhost:81/industrialsafetytrainers/wp-content/uploads/2017/08/whats-new-300x56.png 300w, http://localhost:81/industrialsafetytrainers/wp-content/uploads/2017/08/whats-new-768x144.png 768w, http://localhost:81/industrialsafetytrainers/wp-content/uploads/2017/08/whats-new-1024x192.png 1024w" sizes="(max-width: 2000px) 100vw, 2000px">
+		</section>
+
+		<div class="container blog-container">
+			<aside class="sidebar">
+				<?php
+					dynamic_sidebar('blog-sidebar');
+				?>
+			</aside>
+			<section class="posts">
+				<?php 
+				if (have_posts()): while (have_posts()) : the_post(); ?>
+					<article class="post d-flex">
+						<div class="<?php if(has_post_thumbnail()){ echo 'has-thumbnail'; } ?>">
+							<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+							<?php
+								if(has_post_thumbnail()){
+									echo '<a href="'.get_the_permalink().'">';
+										the_post_thumbnail();
+									echo '</a>';
+								}
+							?>
+
+							<?php the_excerpt(); ?>
+							<div class="post-meta d-flex">
+								<span style="margin-right: 10px;"><?php the_date('M d, Y'); ?></span>
+								<span><?php echo the_category(', '); ?></span>
+							</div>
+						</div>
+					</article>
+					<?php 
+					endwhile; ?>
+				<?php endif; ?>
+			</section>
+		</div>
+
+
+		<div class="container d-flex justify-content-center">
+			<div class="nav-next"><?php previous_posts_link( '<span class="btn btn-primary left-arrow"></span> Previous Page' ); ?></div>
+			<div class="nav-previous"><?php next_posts_link( 'Next Page <span class="btn btn-primary right-arrow"></span>' ); ?></div>
+		</div>
+
+
+
+	</main>
+
+<?php echo do_shortcode('[common_element id="55" name="CTA"]'); ?>
+<?php echo do_shortcode('[common_element id="52" name="About Industrial Safety Trainers"]'); ?>
+	
+<?php get_footer(); ?>
