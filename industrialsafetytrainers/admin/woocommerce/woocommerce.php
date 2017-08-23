8371ba8fb78eafbd59b3dbf462fcea4830d1d843
andrew-macpherson <andrewmac17@gmail.com>
2017-08-23 16:56:42 -0400

    end of day updates

--- industrialsafetytrainers/admin/woocommerce/woocommerce.php
+++ industrialsafetytrainers/admin/woocommerce/woocommerce.php
@@ -29,9 +29,14 @@ function register_course_product_type() {
 			$this->product_type = 'course';
 			parent::__construct( $product );
 		}
+
+		public function get_type() {
+			return 'course';
+		}
+
 	}
 }
-add_action( 'plugins_loaded', 'register_course_product_type' );
+add_action( 'init', 'register_course_product_type' );
 
 
 /**
@@ -39,7 +44,7 @@ add_action( 'plugins_loaded', 'register_course_product_type' );
  */
 function add_course_product( $types ){
 	// Key should be exactly the same as in the class
-	$types[ 'course' ] = __( 'Course' );
+	$types['course'] = __( 'Course' );
 	return $types;
 }
 add_filter( 'product_type_selector', 'add_course_product' );
@@ -55,8 +60,10 @@ function course_custom_js() {
 	?>
 		<script type='text/javascript'>
 			jQuery( document ).ready( function() {
-				jQuery( '.options_group.pricing' ).addClass( 'show_if_course' ).show();
-
+				jQuery('.options_group.pricing').addClass( 'show_if_course' ).show();
+				jQuery('.general_options.general_tab').addClass( 'show_if_course' ).show();
+				jQuery('.attribute_options.attribute_tab').addClass( 'show_if_course' ).show();
+				jQuery('.variations_options.variations_tab').addClass( 'show_if_course' ).show();
 
 
 			});
@@ -86,144 +93,98 @@ add_filter( 'woocommerce_product_data_tabs', 'custom_product_tabs' );
  * Contents of the course options product tab.
  */
 function course_options_product_tab_content() {
-	global $post;
+	global $woocommerce, $product, $post;
 	?>
 	<div id='course_options' class='panel woocommerce_options_panel'>
 		<div class='options_group'>
 
-		<div style="padding: 20px;">
-			<button class="button button-primary">Add Location</button>
-		</div>
-
-		<div style="padding:20px;">
-			<div style="margin-bottom: 50px;">
-				<p style="text-align: right;"><input name="" value="location 1" /> - <button class="button button-primary">Add Date</button></p>
-				<div>
-					<table class="widefat fixed">
-						<thead>
-							<tr>
-								<th>Date</th>
-								<th class="num">Signed Up</th>
-								<th>Maximum</th>
-								<th class="num">Export</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td>2017-09-01</td>
-								<td class="num">2</td>
-								<td><input name="" value="10" /></td>
-								<td class="num"><button class="button button-primary">Export Attendees</button></td>
-							</tr>
-							<tr>
-								<td>2017-09-02</td>
-								<td class="num">6</td>
-								<td><input name="" value="10" /></td>
-								<td class="num"><button class="button button-primary">Export Attendees</button></td>
-							</tr>
-							<tr>
-								<td>2017-09-03</td>
-								<td class="num">4</td>
-								<td><input name="" value="10" /></td>
-								<td class="num"><button class="button button-primary">Export Attendees</button></td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
+			<div style="padding: 20px;">
+				<button class="button button-primary">Add Location</button>
 			</div>
 
-			<div style="margin-bottom: 50px;">
-				<p style="text-align: right;"><input name="" value="location 1" /> - <button class="button button-primary">Add Date</button></p>
-				<div>
-					<table class="widefat fixed">
-						<thead>
-							<tr>
-								<th>Date</th>
-								<th class="num">Signed Up</th>
-								<th>Maximum</th>
-								<th class="num">Export</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td>2017-09-01</td>
-								<td class="num">2</td>
-								<td><input name="" value="10" /></td>
-								<td class="num"><button class="button button-primary">Export Attendees</button></td>
-							</tr>
-							<tr>
-								<td>2017-09-02</td>
-								<td class="num">6</td>
-								<td><input name="" value="10" /></td>
-								<td class="num"><button class="button button-primary">Export Attendees</button></td>
-							</tr>
-							<tr>
-								<td>2017-09-03</td>
-								<td class="num">4</td>
-								<td><input name="" value="10" /></td>
-								<td class="num"><button class="button button-primary">Export Attendees</button></td>
-							</tr>
-						</tbody>
-					</table>
+
+			<?php 
+			$product_variations = new WC_Product_Variable($post->ID);
+			$variables = $product_variations->get_available_variations();
+			print_r($variables);
+
+			//$available_variations = $product->get_available_variations();
+			//print_r($available_variations);
+
+
+
+			?>
+
+			<div style="padding:20px;">
+				<div style="margin-bottom: 50px;">
+					<p style="text-align: right;"><input name="_locations[0]" value="location 1" /> - <button class="button button-primary">Add Date</button></p>
+					<div>
+						<table class="widefat fixed">
+							<thead>
+								<tr>
+									<th>Date</th>
+									<th class="num">Signed Up</th>
+									<th>Maximum</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td><input name="_location_0_dates[]" value="2017-09-01" /></td>
+									<td class="num">2</td>
+									<td><input name="_location_0_dates_max[]" value="10" /></td>
+								</tr>
+								<tr>
+									<td><input name="_location_0_dates[]" value="2017-09-02" /></td>
+									<td class="num">6</td>
+									<td><input name="_location_0_dates_max[]" value="10" /></td>
+								</tr>
+								<tr>
+									<td><input name="_location_0_dates[]" value="2017-09-03" /></td>
+									<td class="num">4</td>
+									<td><input name="_location_0_dates_max[]" value="10" /></td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
 				</div>
-			</div>
 
-			<div style="margin-bottom: 50px;">
-				<p style="text-align: right;"><input name="" value="location 1" /> - <button class="button button-primary">Add Date</button></p>
-				<div>
-					<table class="widefat fixed">
-						<thead>
-							<tr>
-								<th>Date</th>
-								<th class="num">Signed Up</th>
-								<th>Maximum</th>
-								<th class="num">Export</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td>2017-09-01</td>
-								<td class="num">2</td>
-								<td><input name="" value="10" /></td>
-								<td class="num"><button class="button button-primary">Export Attendees</button></td>
-							</tr>
-							<tr>
-								<td>2017-09-02</td>
-								<td class="num">6</td>
-								<td><input name="" value="10" /></td>
-								<td class="num"><button class="button button-primary">Export Attendees</button></td>
-							</tr>
-							<tr>
-								<td>2017-09-03</td>
-								<td class="num">4</td>
-								<td><input name="" value="10" /></td>
-								<td class="num"><button class="button button-primary">Export Attendees</button></td>
-							</tr>
-						</tbody>
-					</table>
+				<div style="margin-bottom: 50px;">
+					<p style="text-align: right;"><input name="_locations[1]" value="location 2" /> - <button class="button button-primary">Add Date</button></p>
+					<div>
+						<table class="widefat fixed">
+							<thead>
+								<tr>
+									<th>Date</th>
+									<th class="num">Signed Up</th>
+									<th>Maximum</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td><input name="_location_1_dates[]" value="2017-09-01" /></td>
+									<td class="num">2</td>
+									<td><input name="_location_0_dates_max[]" value="10" /></td>
+								</tr>
+								<tr>
+									<td><input name="_location_1_dates[]" value="2017-09-02" /></td>
+									<td class="num">6</td>
+									<td><input name="_location_0_dates_max[]" value="10" /></td>
+								</tr>
+								<tr>
+									<td><input name="_location_1_dates[]" value="2017-09-03" /></td>
+									<td class="num">4</td>
+									<td><input name="_location_0_dates_max[]" value="10" /></td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
 				</div>
-			</div>
-		</div>
-
-		
-
-		<?php
-			woocommerce_wp_checkbox( array(
-				'id' 		=> '_enable_renta_option',
-				'label' 	=> __( 'Enable course option X', 'woocommerce' ),
-			) );
-			woocommerce_wp_text_input( array(
-				'id'			=> '_text_input_y',
-				'label'			=> __( 'What is the value of Y', 'woocommerce' ),
-				'desc_tip'		=> 'true',
-				'description'	=> __( 'A handy description field', 'woocommerce' ),
-				'type' 			=> 'text',
-			) );
-		?>
 			
+			</div>
 		</div>
 	</div>
 	<?php
+
 }
 add_action( 'woocommerce_product_data_panels', 'course_options_product_tab_content' );
 
@@ -234,12 +195,110 @@ add_action( 'woocommerce_product_data_panels', 'course_options_product_tab_conte
 /**
  * Save the custom fields.
  */
-function save_course_option_field( $post_id ) {
+function save_course_option_field( $product_id ) {
+
+	if(isset($_POST['_locations'])){
+
+		
+		//CONFIGURE ATTRIBUTES
+		$location = wc_attribute_taxonomy_name('Location');
+		$date = wc_attribute_taxonomy_name('Date');
+
+		$attributes = array(
+			$location => array(
+				'name' => $location,
+				'value' =>'',
+				'is_visible' => '1',
+				'is_variation' => '1',
+				'is_taxonomy' => '1'
+			),
+			$date => array(
+				'name' => $date,
+				'value' => '',
+				'is_visible' => '1',
+				'is_variation' => '1',
+				'is_taxonomy' => '1'
+			)
+		);
+
+
+		//SET PRODUCT ATTRIBUTES
+		update_post_meta( $product_id, '_product_attributes', $attributes );
+
+
+
+
+		// Assign location and dates to the main product
+
+		$locationsArray = array();
+		$datesArray = array();
+		foreach($_POST['_locations'] as $l_key => $l_val){
+			$locationsArray[] = $l_val;
+			
+			if(isset($_POST['_location_'.$l_key.'_dates'])){
+				foreach($_POST['_location_'.$l_key.'_dates'] as $d_key => $d_val){
+					$datesArray[] = $d_val;
+				}
+			}
+		}
+
+
+		wp_set_object_terms($product_id, $locationsArray, $location );
+		wp_set_object_terms($product_id, $datesArray, $date );
+
+		
+		$parent_id = $product_id;
+
+		// GET CURRENT VARIATIONS AND MAKE SURE WE DONT RE ADD. 
+		$product_variations = new WC_Product_Variable($parent_id);
+		$variables = $product_variations->get_available_variations();
+		print_r($variables);
+
+
+		foreach($_POST['_locations'] as $l_key => $l_val){
+			if(isset($_POST['_location_'.$l_key.'_dates'])){
+				foreach($_POST['_location_'.$l_key.'_dates'] as $d_key => $d_val){
+
+					$variation = array(
+					    'post_title'   => 'Product #' . $parent_id . ' location: '.$l_val.' date: '.$d_val.'',
+					    'post_content' => '',
+					    'post_status'  => 'publish',
+					    'post_parent'  => $parent_id,
+					    'post_type'    => 'product_variation'
+					);
+					// The variation id
+					$variation_id = wp_insert_post( $variation );
+
+					// Regular Price ( you can set other data like sku and sale price here )
+					update_post_meta( $variation_id, '_regular_price', 2 );
+					update_post_meta( $variation_id, '_price', 2 );
+
+					// Assign the size and color of this variation
+					$location_value = strtolower($l_val);
+					$location_value = str_replace(' ', '-', $location_value);
+
+					$date_value = strtolower($d_val);
+					$date_value = str_replace(' ', '-', $date_value);
+
+					update_post_meta( $variation_id, 'attribute_' . $location, $location_value );
+					update_post_meta( $variation_id, 'attribute_' . $date, $date_value );
+				}
+			}
+		}
+
+		// Update parent if variable so price sorting works and stays in sync with the cheapest child
+		WC_Product_Variable::sync( $parent_id );
+	}
+
+	/*
 	$course_option = isset( $_POST['_enable_renta_option'] ) ? 'yes' : 'no';
 	update_post_meta( $post_id, '_enable_renta_option', $course_option );
+
+
 	if ( isset( $_POST['_text_input_y'] ) ) :
 		update_post_meta( $post_id, '_text_input_y', sanitize_text_field( $_POST['_text_input_y'] ) );
 	endif;
+	*/
 }
 add_action( 'woocommerce_process_product_meta_course', 'save_course_option_field'  );
 add_action( 'woocommerce_process_product_meta_variable_course', 'save_course_option_field'  );
