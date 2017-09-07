3c1e789bbf6b7f442ca96028bc97e10f9ca16bb3
andrew-macpherson <andrewmac17@gmail.com>
2017-09-07 11:44:17 -0400

    fix sticky header

--- industrialsafetytrainers/src/js/scripts.js
+++ industrialsafetytrainers/src/js/scripts.js
@@ -15,7 +15,8 @@ function elementInViewport(element) {
 
 function checkOnScrollElements(){
 	//Sticky Header
-	var docTop = document.body.scrollTop;
+	var docTop = document.documentElement.scrollTop;
+	console.log(docTop);
 	if(docTop >= 25){
 		document.body.classList.add("sticky-header");
 	}else{
